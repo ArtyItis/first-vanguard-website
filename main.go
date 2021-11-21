e8d84a9dc6f862842083b618c98432d6afaec344
Craxlor <arnaud.kalthoff@web.de>
2021-11-21 23:56:24 +0100

    changed template files from html to tmpl

--- main.go
+++ main.go
@@ -1,29 +1,13 @@
 package main
 
 import (
-	"forgottennw/app/controller"
+	"forgottennw/app/route"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	//exclude static prefix from url
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	r := mux.NewRouter()
-	http.Handle("/", r)
-	// Create a file server which serves files out of the "../static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("../static/"))
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	r.Handle("/static/", http.StripPrefix("/static", fileServer))
-	r.HandleFunc("/", controller.Index).Methods("GET")
-	r.HandleFunc("/recruitment", controller.RecruitmentGET).Methods("GET")
-	r.HandleFunc("/recruitment", controller.RecruitmentPOST).Methods("POST")
+	route.MapURLs()
 	log.Println("Listening...")
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8080", nil)
 }
