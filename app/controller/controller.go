e95e3a4090ca58dc957cf98ccc770b1daf346b5f
Craxlor <arnaud.kalthoff@web.de>
2021-11-25 14:49:03 +0100

    added ParseInt()

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 var (
@@ -15,4 +16,9 @@ var (
 func Index(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("template/index.html", head, navigation, footer))
 	tmpl.Execute(w, nil)
+}
+
+func ParseInt(str string) (result int) {
+	result, _ = strconv.Atoi(str)
+	return result
 }
