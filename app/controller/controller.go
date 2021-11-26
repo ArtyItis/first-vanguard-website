6d1c180d7c886317002fce4172b12f2d1aa895ef
Craxlor <arnaud.kalthoff@web.de>
2021-11-26 19:25:48 +0100

    created Data & Session struct implemented GetSessionInformation() & GetCurrentDate()

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -1,21 +1,72 @@
 package controller
 
 import (
-	"html/template"
+	"crypto/rand"
+	"forgottennw/app/model"
+	"log"
 	"net/http"
 	"strconv"
+	"time"
+
+	"github.com/gorilla/sessions"
 )
 
 var (
-	tmpl       *template.Template
+	store      *sessions.CookieStore
 	head       = "template/head.html"
 	navigation = "template/navigation.html"
 	footer     = "template/footer.html"
 )
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("template/index.html", head, navigation, footer))
-	tmpl.Execute(w, nil)
+type Data struct {
+	Session             Session
+	Recruitment_Entries []map[string]interface{}
+}
+
+type Session struct {
+	LoggedIn bool
+	User     model.User
+}
+
+func init() {
+	key := make([]byte, 32)
+	rand.Read(key)
+	store = sessions.NewCookieStore(key)
+}
+
+func GetSessionInformation(r *http.Request) (sessionInfo Session) {
+	session, _ := store.Get(r, "session")
+	username := ""
+	loggedIn := false
+	if session.Values["username"] != nil {
+		username = session.Values["username"].(string)
+	}
+	if session.Values["authenticated"] != nil {
+		loggedIn = session.Values["authenticated"].(bool)
+	}
+	user, user_err := model.GetUserByName(username)
+	if user_err != nil {
+		log.Println("couldn't find user in getSessionInformation()")
+	}
+	sessionInfo = Session{
+		LoggedIn: loggedIn,
+		User:     user,
+	}
+	return sessionInfo
+}
+
+func GetCurrentDate() (date model.Date) {
+	date_Now := time.Now()
+	date = model.Date{
+		Date:   date_Now.Local(),
+		Second: date_Now.Second(),
+		Minute: date_Now.Minute(),
+		Hour:   date_Now.Hour(),
+		Day:    date_Now.Day(),
+		Month:  int(date_Now.Month()),
+		Year:   date_Now.Year(),
+	}
+	return date
 }
 
 func ParseInt(str string) (result int) {
