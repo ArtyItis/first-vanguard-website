de9a1ba2e8a9954797568f5a60530f2e8c34501c
Craxlor <arnaud.kalthoff@web.de>
2021-11-26 19:17:51 +0100

    merged simple handlers in handler.go

---
+++ app/controller/handler.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func ExecuteTemplate(w http.ResponseWriter, r *http.Request, fileName string) {
+	tmpl := template.Must(template.ParseFiles("template/"+fileName+".html", head, navigation, footer))
+	data := Data{
+		Session: GetSessionInformation(r),
+	}
+	tmpl.Execute(w, data)
+}
+
+func IndexGET(w http.ResponseWriter, r *http.Request) {
+	ExecuteTemplate(w, r, "index")
+}
+
+func ImprintGET(w http.ResponseWriter, r *http.Request) {
+	ExecuteTemplate(w, r, "imprint")
+}
+
+func NewsGET(w http.ResponseWriter, r *http.Request) {
+	ExecuteTemplate(w, r, "news")
+}
+
+func AboutGET(w http.ResponseWriter, r *http.Request) {
+	ExecuteTemplate(w, r, "about")
+}
