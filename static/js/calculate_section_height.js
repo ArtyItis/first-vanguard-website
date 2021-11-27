cd5680f935b3b62d736276d031ef02b31718d862
Craxlor <arnaud.kalthoff@web.de>
2021-11-27 15:26:48 +0100

    wrapped code in function

--- static/js/calculate_section_height.js
+++ static/js/calculate_section_height.js
@@ -1,16 +1,21 @@
+window.addEventListener("load", calculateSectionHeight);
+// window.addEventListener("orientationchange", calculateSectionHeight);
+// window.addEventListener("resize", calculateSectionHeight);
 
-var nav_h = document.querySelector("nav").offsetHeight;
-var foo_h = document.querySelector("footer").offsetHeight;
+function calculateSectionHeight() {
+    var nav_h = document.querySelector("nav").offsetHeight;
+    var foo_h = document.querySelector("footer").offsetHeight;
 
-var viewport_h = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    var viewport_h = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-var content_section = document.getElementById("content");
-var content_section_margin_top = parseInt(window.getComputedStyle(content_section).marginTop);
+    var content_section = document.getElementById("content");
+    var content_section_margin_top = parseInt(window.getComputedStyle(content_section).marginTop);
 
-var content_section_h = viewport_h - (nav_h + foo_h + content_section_margin_top);
+    var content_section_h = viewport_h - (nav_h + foo_h + content_section_margin_top);
 
-var current_content_section_h = parseInt(window.getComputedStyle(content_section).height);
+    var current_content_section_h = parseInt(window.getComputedStyle(content_section).height);
 
-if (current_content_section_h < content_section_h) {
-    content_section.style.height = content_section_h + "px";
+    if (current_content_section_h < content_section_h) {
+        content_section.style.height = content_section_h + "px";
+    }
 }
