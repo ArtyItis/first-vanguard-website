3111bdcda7defbc798ae160632ac12d22c915fcd
Craxlor <arnaud.kalthoff@web.de>
2021-11-27 15:24:02 +0100

    changed Date.Date from time.Time to string

--- app/model/model.go
+++ app/model/model.go
@@ -1,8 +1,6 @@
 package model
 
 import (
-	"time"
-
 	couchdb "github.com/leesper/couchdb-golang"
 )
 
@@ -13,11 +11,11 @@ var (
 )
 
 type Date struct {
-	Date   time.Time `json:"Date"`
-	Hour   int       `json:"hour"`
-	Minute int       `json:"minute"`
-	Second int       `json:"second"`
-	Day    int       `json:"day"`
-	Month  int       `json:"month"`
-	Year   int       `json:"year"`
+	Date   string `json:"Date"`
+	Hour   int    `json:"hour"`
+	Minute int    `json:"minute"`
+	Second int    `json:"second"`
+	Day    int    `json:"day"`
+	Month  int    `json:"month"`
+	Year   int    `json:"year"`
 }
