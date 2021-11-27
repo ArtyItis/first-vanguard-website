b414f73aec5cb2131ea26632f1768afbe314bc82
Craxlor <arnaud.kalthoff@web.de>
2021-11-27 15:26:23 +0100

    added routing for members updated routing for recruitments

--- app/route/route.go
+++ app/route/route.go
@@ -14,10 +14,19 @@ func NewRouter() *mux.Router {
 	router.PathPrefix(staticDir).Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir("."+staticDir))))
 
 	router.HandleFunc("/", controller.IndexGET).Methods("GET")
-	router.HandleFunc("/recruitment", controller.RecruitmentGET).Methods("GET")
-	router.HandleFunc("/recruitment", controller.RecruitmentPOST).Methods("POST")
 
-	router.HandleFunc("/news", controller.NewsGET).Methods("GET")
+	router.HandleFunc("/recruitment", controller.RecruitmentFormGET).Methods("GET")
+	router.HandleFunc("/recruitment", controller.RecruitmentFormPOST).Methods("POST")
+
+	router.HandleFunc("/recruitments", controller.Authenticate(controller.RecruitmentsGET)).Methods("GET")
+	router.HandleFunc("/recruitments/{id}", controller.Authenticate(controller.RecruitmentEntryGET)).Methods("GET")
+	router.HandleFunc("/recruitments/{id}/accepted", controller.Authenticate(controller.RecruitmentEntryAcceptedPOST)).Methods("POST")
+	router.HandleFunc("/recruitments/{id}/rejected", controller.Authenticate(controller.RecruitmentEntryRejectedPOST)).Methods("POST")
+
+	router.HandleFunc("/members", controller.UsersGET).Methods("GET")
+	router.HandleFunc("/members/{id}", controller.UserGET).Methods("GET")
+	router.HandleFunc("/members/{id}/changePassword", controller.Authenticate(controller.ChangePasswordGET)).Methods("GET")
+	router.HandleFunc("/members/{id}/changePassword", controller.Authenticate(controller.ChangePasswordPOST)).Methods("POST")
 
 	router.HandleFunc("/imprint", controller.ImprintGET).Methods("GET")
 	router.HandleFunc("/about_us", controller.AboutGET).Methods("GET")
@@ -27,5 +36,7 @@ func NewRouter() *mux.Router {
 
 	router.HandleFunc("/register", controller.RegisterGET).Methods("GET")
 	router.HandleFunc("/register", controller.RegisterPOST).Methods("POST")
+	// TODO:  /members, /members/{id} -> change password
+	// TODO: sort options /recruitments, /members
 	return router
 }
