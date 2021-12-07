0fb8ef61fb526c3dfec1f9b40a7bcebbf148f42b
Craxlor <arnaud.kalthoff@web.de>
2021-12-07 16:04:18 +0100

    removed ContainsWeapon(), ContainsRole() moved ConvertPermissionLevelUser() to user.go

--- app/controller/templateFunctions.go
+++ app/controller/templateFunctions.go
@@ -12,45 +12,17 @@ func Minus(x, y int) int {
 	return x - y
 }
 
-func ContainsWeapon(w map[string]interface{}, weapons []model.Weapon) bool {
-	weapon := model.Map2Weapon(w)
-	for _, w := range weapons {
-		if w.Name == weapon.Name {
-			return true
-		}
-	}
-	return false
-}
-
-func ContainsRole(r map[string]interface{}, roles []model.Role) bool {
-	role := model.Map2Role(r)
-	for _, w := range roles {
-		if w.Name == role.Name {
-			return true
-		}
-	}
-	return false
-}
-
 func ConvertPermissionLevelMap(u map[string]interface{}) string {
 	user := model.Map2User(u)
-	return ConvertPermissionLevelUser(user)
+	return user.ConvertPermissionLevel()
 }
 
-func ConvertPermissionLevelUser(user model.User) string {
-	switch user.Permission_level {
-	case 0:
-		return "Siedler"
-	case 200:
-		return "Offizier"
-	case 400:
-		return "Konsul"
-	case 450:
-		return "Konsul/Inkasso"
-	case 600:
-		return "Gouverneur"
-	case 1000:
-		return "Website-Admin"
-	}
-	return "nicht definiert"
+func GetRoleName(roleID string) string {
+	role, _ := model.GetRoleById(roleID)
+	return role.Name
+}
+
+func GetWeaponName(weaponID string) string {
+	weapon, _ := model.GetWeaponById(weaponID)
+	return weapon.Name
 }
