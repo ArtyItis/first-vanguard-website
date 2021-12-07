0b17f77d318fa05a155f9aa6e0a6d76e2c4cf18c
Craxlor <arnaud.kalthoff@web.de>
2021-12-07 16:06:47 +0100

    changed type of Roles & Weapons element

--- app/model/character.go
+++ app/model/character.go
@@ -5,13 +5,12 @@ type Character struct {
 	Gearscore        int          `json:"gearscore"`
 	Equipment_weight string       `json:"equipment_weight"`
 	Attributes       Attributes   `json:"attributes"`
-	Roles            []Role       `json:"roles"`
-	Weapons          []Weapon     `json:"weapons"`
+	Roles            []string     `json:"roles"`
+	Weapons          []string     `json:"weapons"`
 	Old_company      string       `json:"old_company"`
 	Crafting_jobs    CraftingJobs `json:"crafting_jobs"`
 	Refining_jobs    RefiningJobs `json:"refining_jobs"`
 }
-
 type Attributes struct {
 	Strength     int `json:"strength"`
 	Dexterity    int `json:"dexterity"`
@@ -19,7 +18,6 @@ type Attributes struct {
 	Focus        int `json:"focus"`
 	Constitution int `json:"constitution"`
 }
-
 type CraftingJobs struct {
 	Armoring       bool `json:"armoring"`
 	Weaponsmithing bool `json:"weaponsmithing"`
