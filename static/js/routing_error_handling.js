d798a4468cb9a3dff0362ca91253c11573a4f72e
Craxlor <arnaud.kalthoff@web.de>
2021-12-09 21:53:52 +0100

    updated error handling to be more understandable

--- static/js/routing_error_handling.js
+++ static/js/routing_error_handling.js
@@ -1,47 +1,74 @@
-var url = new URL(window.location.href);
-loginError();
-passwordChangeError();
-applicationSuccess();
+const url = new URL(window.location.href);
+const error = url.searchParams.get("error");
+const type = url.searchParams.get("type");
+const success = url.searchParams.get("success");
 
-function loginError() {
-    var loginError = url.searchParams.get("loginError");
-    if (loginError != null) {
-        var loginModal = document.getElementById("loginModal");
-        var modal = bootstrap.Modal.getOrCreateInstance(loginModal)
-        var loginModalLabel = document.getElementById("loginModalLabel");
-        if (loginError == "password") {
-            loginModalLabel.classList.add("text-danger");
-            loginModalLabel.innerHTML = "Passwort wurde falsch eingegeben";
-        } else if (loginError == "username") {
-            loginModalLabel.classList.add("text-danger");
-            loginModalLabel.innerHTML = "username ist falsch";
+switch (error) {
+    case "authentication":
+        if (type == "login") {
+            loginError();
         }
-        modal.show();
-    }
+        break;
+    case "login":
+        loginError();
+        break;
+    case "password":
+        passwordError();
+        break;
+    case "user":
+
+        break;
+    default:
+        break;
 }
 
-function passwordChangeError() {
-    var passwordChangeError = url.searchParams.get("changePasswordError");
-    if (passwordChangeError != null) {
-        var content = document.getElementById("content");
-        var msg;
-        var div = document.createElement("div");
-        if (passwordChangeError == "newPassword") {
-            msg = "neues Passwort wurde falsch wiederholt"
-        } else if (passwordChangeError == "oldPassword") {
-            msg = "altes Passwort ist inkorrekt"
-        }
-        div.innerHTML = msg;
-        div.classList.add("text-danger");
-        content.insertBefore(div, content.firstChild);
+switch (success) {
+    case "application":
+        applicationSuccess();
+        break;
+    case "passwordChange":
+
+        break;
+}
+
+function loginError() {
+    let loginModal = document.getElementById("loginModal");
+    let loginModalInstance = bootstrap.Modal.getOrCreateInstance(loginModal);
+    let loginModalLabel = document.getElementById("loginModalLabel");
+    loginModalLabel.classList.add("text-danger");
+    switch (type) {
+        case "username":
+            loginModalLabel.innerHTML = "username ist falsch";
+            break;
+        case "password":
+            loginModalLabel.innerHTML = "Passwort wurde falsch eingegeben";
+            break;
     }
+    loginModalInstance.show();
+}
+
+function passwordError() {
+    let content = document.getElementById("content");
+    let msg;
+    let div = document.createElement("div");
+    switch (type) {
+        case "old":
+            msg = "altes Passwort ist inkorrekt"
+            break;
+        case "new":
+            msg = "neues Passwort wurde falsch wiederholt"
+            break;
+        default:
+            break;
+    }
+    div.innerHTML = msg;
+    div.classList.add("text-danger");
+    content.insertBefore(div, content.firstChild);
+
 }
 
 function applicationSuccess() {
-    var applicationSuccess = url.searchParams.get("application");
-    if (applicationSuccess == "success") {
-        var myToastEl = document.getElementById('applicationSuccessToast')
-        var myToast = bootstrap.Toast.getOrCreateInstance(myToastEl)
-        myToast.show();
-    }
+    let myToastEl = document.getElementById('applicationSuccessToast')
+    let myToast = bootstrap.Toast.getOrCreateInstance(myToastEl)
+    myToast.show();
 }
