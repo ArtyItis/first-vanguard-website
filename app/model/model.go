c9f9a0bdbf79631434d455aff071da869b42a6ad
Craxlor <arnaud.kalthoff@web.de>
2021-12-28 17:32:57 +0100

    added contains(), CleanUpFiles()

--- app/model/model.go
+++ app/model/model.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"log"
+	"os"
+
 	couchdb "github.com/leesper/couchdb-golang"
 )
 
@@ -20,4 +23,35 @@ type Date struct {
 	Month  int `json:"month"`
 	Year   int `json:"year"`
 	Week   int `json:"week"`
+}
+
+//classic contain method for arrays
+//implemented for string arrays
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+use this for file deletion. Look at task_handler.go EditTaskUpload() DeleteTask() for an example
+param files should contain a string array which contains all currently used filepaths
+param filePath should contain the filePath of the replaced file
+*/
+func CleanUpFiles(files []string, filePath string) {
+	if filePath == "" {
+		return
+	}
+	log.Println(filePath)
+	if contains(files, filePath) == false {
+		filePath = filePath[1:len(filePath)]
+		log.Println("no more references detected. deleting " + filePath)
+		err := os.Remove(filePath) //deleting file
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
